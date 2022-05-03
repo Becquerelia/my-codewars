803245c29745228262ffbc334e3fedc0d1daabd2
Eva G Cagigas <evarteca@gmail.com>
2022-05-03 20:09:57 +0200

    first file

---
+++ 8 kyu/AreYouPlayingBanjo.js
@@ -0,0 +1,24 @@
+ 
+ ```javascript
+ const aboutMe = {
+  
+   city: "Madrid",
+   code: ["JavascriptES6", "HTML5", "CSS3"],
+   technologies: ["React", "Node.js", "ExpressJS", "Handlebars", "Bootstrap", "MUI"],
+   backEndTools: ["RESTful API", "Axios", "Postman"],
+   dataBase: ["MongoDB", "Mongoose", "noSQL"],
+   otherPackages: ["Stripe", "Socket.io", "FullCalendar", "Nodemailer", "Cloudinary", "GoogleMaps Platform"],
+   versionControl: ["gitHub", "Netlify", "Heroku"],
+  
+   webDevEducation: {
+                         teachInstitution: "IronHack",
+                         course: "Full Stack Web Developer",
+                         format: "Full Time",
+                         location: "remote"
+                       },
+  
+  preference: "Front-end"
+ }
+ ```
+  
+ 
